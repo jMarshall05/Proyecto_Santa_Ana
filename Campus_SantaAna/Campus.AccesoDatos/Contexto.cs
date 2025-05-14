c131a0ba9a8b4fa21797c12f0d9a00c27e24096c
jMarshall05 <jmarshall40938@ufide.ac.cr>
2025-05-14 01:06:46 -0600

    Estructura basica  y identity

---
+++ Campus_SantaAna/Campus.AccesoDatos/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campus.AccesoDatos
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base("name=Cpntexto")
+        {
+        }
+    
+
+    }
+}
