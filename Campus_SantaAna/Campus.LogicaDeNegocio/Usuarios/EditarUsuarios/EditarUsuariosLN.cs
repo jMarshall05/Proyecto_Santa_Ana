03f934ca1b38f7d79e5cd4fe291a78d7b02f177f
jMarshall05 <jmarshall40938@ufide.ac.cr>
2025-05-26 20:30:26 -0600

    Añadir funcionalidad de edición de usuarios

---
+++ Campus_SantaAna/Campus.LogicaDeNegocio/Usuarios/EditarUsuarios/EditarUsuariosLN.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Campus.Abstracciones.AccesoDatos.Usuarios.EditarUsuariosAD;
+using Campus.Abstracciones.LogicaDeNegocio.Usuarios.EditarUsuariosLN;
+using Campus.Abstracciones.ModelosUI;
+using Campus.AccesoDatos.Usuarios.EditarUsuariosAD;
+
+namespace Campus.LogicaDeNegocio.Usuarios.EditarUsuarios
+{
+    public class EditarUsuariosLN : IEditarUsuarioLN
+    {
+        private IEditarUsuarioAD _editarUsuario;
+        public EditarUsuariosLN()
+        {
+            _editarUsuario = new EditarUsuarioAD();
+        }
+        public Task<int> EditarUsuario(string id, UsuariosDto usuario)
+        {
+            return _editarUsuario.EditarUsuario(id, usuario);
+        }
+    }
+}
