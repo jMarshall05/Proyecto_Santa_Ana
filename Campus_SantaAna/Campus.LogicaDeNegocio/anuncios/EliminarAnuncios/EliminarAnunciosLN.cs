719d38c349b114418c4f2e09d3bfbd3bbdb7ed5c
aloncabezas <aloncm04@gmail.com>
2025-05-27 19:19:27 -0600

    El Agregar, eliminar, editar y visualizar los  anuncios ademas de esto el poder visualizar los documentos importantes del colegio

---
+++ Campus_SantaAna/Campus.LogicaDeNegocio/anuncios/EliminarAnuncios/EliminarAnunciosLN.cs
@@ -0,0 +1,21 @@
+﻿using Campus.Abstracciones.AccesoDatos.Anuncios.EliminarAnunciosAD;
+using Campus.Abstracciones.LogicaDeNegocio.Anuncios.EliminarAnunciosLN;
+using Campus.AccesoDatos.Anuncios.EliminarAnunciosAD;
+
+namespace Campus.LogicaDeNegocio.Anuncios.EliminarAnuncios
+{
+    public class EliminarAnunciosLN : IEliminarAnunciosLN
+    {
+        private readonly IEliminarAnunciosAD _eliminarAnunciosAD;
+
+        public EliminarAnunciosLN()
+        {
+            _eliminarAnunciosAD = new EliminarAnunciosAD();
+        }
+
+        public void EliminarAnuncio(int anuncioId)
+        {
+            _eliminarAnunciosAD.EliminarAnuncio(anuncioId);
+        }
+    }
+}
