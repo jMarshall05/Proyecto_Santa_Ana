beba0df7fba502f069f20de924c5728fc406be05
aloncabezas <aloncm04@gmail.com>
2025-06-13 19:06:52 -0600

    Cambios en la base de datos agregar tablas de EstudianteGrupo, ProfesorCurso,Curso y cambios en la tabla de Materias

---
+++ Campus_SantaAna/BD/dbo/Tables/EstudianteGrupo.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[EstudianteGrupo] (
+    [IdEstudianteGrupo] INT            IDENTITY (1, 1) NOT NULL,
+    [EstudianteId]      NVARCHAR (128) NOT NULL,
+    [GrupoId]           INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([IdEstudianteGrupo] ASC),
+    CONSTRAINT [FK_EstudianteGrupo_Estudiante] FOREIGN KEY ([EstudianteId]) REFERENCES [dbo].[Usuarios_tb] ([IdUsuario]) ON DELETE CASCADE,
+    CONSTRAINT [FK_EstudianteGrupo_Grupo] FOREIGN KEY ([GrupoId]) REFERENCES [dbo].[grupos] ([id_grupo])
+);
+
