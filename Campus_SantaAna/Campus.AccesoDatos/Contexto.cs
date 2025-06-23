3307983b6efda49c473ef23c55b2c5c07bd91bef
aloncabezas <aloncm04@gmail.com>
2025-06-23 12:48:21 -0600

    VLa paret de contactenos filtrada pro profesores y administradores y con uan barra de busqueda

--- Campus_SantaAna/Campus.AccesoDatos/Contexto.cs
+++ Campus_SantaAna/Campus.AccesoDatos/Contexto.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Campus.Abstracciones.ModelosUI;
 using Campus.AccesoDatos.ModelosAD;
 
 namespace Campus.AccesoDatos
@@ -19,6 +20,9 @@ namespace Campus.AccesoDatos
         public DbSet<TareasAD> Tareas { get; set; }
         public DbSet<GruposAD> Grupos { get; set; }
         public DbSet<MateriasAD> Materias { get; set; }
+
+       
+
 
 
     }
