38e48b4497b8a2493d0b73002c10006caac96240
KarenVCS07 <kcamacho20881@ufide.ac.cr>
2025-07-15 13:17:24 -0600

    modulo calificacion 2

---
+++ Campus_SantaAna/Campus.UI/Controllers/CalificacionesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Campus.Abstracciones.LogicaDeNegocio.calificaciones.agregarCalificacionLN;
+using Campus.Abstracciones.LogicaDeNegocio.calificaciones.editarCalificacionLN;
+using Campus.Abstracciones.LogicaDeNegocio.calificaciones.eliminarCalificacionLN;
+using Campus.Abstracciones.LogicaDeNegocio.calificaciones.listarCalificacionLN;
+using Campus.Abstracciones.ModelosUI;
+using Campus.LogicaDeNegocio.calificaciones;
+using Campus.LogicaDeNegocio.calificaciones.eliminarCalificacionLN;
+using Campus.LogicaDeNegocio.calificaciones.listarCalificacionesLN;
+using Microsoft.AspNet.Identity;
+
+namespace Campus.Web.Controllers
+{
+    public class CalificacionesController : Controller
+    {
+        private readonly IAgregarCalificacionLN _agregarCalificacionLN;
+        private readonly IEditarCalificacionLN _editarCalificacionLN;
+        private readonly IEliminarCalificacionLN _eliminarCalificacionLN;
+        private readonly IListarCalificacionesLN _listarCalificacionesLN;
+
+        public CalificacionesController()
+        {
+            _agregarCalificacionLN = new AgregarCalificacionLN();
+            _editarCalificacionLN = new EditarCalificacionLN();
+            _eliminarCalificacionLN = new EliminarCalificacionLN();
+            _listarCalificacionesLN = new ListarCalificacionesLN();
+        }
+
+        public CalificacionesController(
+            IAgregarCalificacionLN agregarCalificacionLN,
+            IEditarCalificacionLN editarCalificacionLN,
+            IEliminarCalificacionLN eliminarCalificacionLN,
+            IListarCalificacionesLN listarCalificacionesLN)
+        {
+            _agregarCalificacionLN = agregarCalificacionLN;
+            _editarCalificacionLN = editarCalificacionLN;
+            _eliminarCalificacionLN = eliminarCalificacionLN;
+            _listarCalificacionesLN = listarCalificacionesLN;
+        }
+
+        public async Task<ActionResult> Index(int? idGrupo)
+        {
+            if (idGrupo == null)
+            {
+                var lista = await _listarCalificacionesLN.ListarCalificaciones();
+                return View(lista);
+            }
+            else
+            {
+                var lista = await _listarCalificacionesLN.ListarCalificacionesPorGrupoAsync(idGrupo.Value);
+                return View(lista);
+            }
+        }
+
+
+        // GET: calificar
+        [HttpGet]
+        public ActionResult Create(int id)
+        {
+            var modelo = new CalificacionesDto
+            {
+                id_entrega = id,
+                fecha_calificacion = DateTime.Now
+            };
+            return View(modelo); 
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(CalificacionesDto calificacion)
+        {
+            if (ModelState.IsValid)
+            {
+                await _agregarCalificacionLN.AgregarCalificacion(calificacion);
+                return RedirectToAction("Index", "Entregas"); 
+            }
+
+            return View(calificacion);
+        }
+
+
+
+        //Edit calificacion
+        public async Task<ActionResult> Edit(int id)
+        {
+            var calificaciones = (await _listarCalificacionesLN.ListarCalificaciones()).ToList();
+            var calificacion = calificaciones.FirstOrDefault(e => e.id_calificacion == id);
+
+            if (calificacion == null)
+                return HttpNotFound();
+
+            return View(calificacion);
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(CalificacionesDto calificacion)
+        {
+            if (ModelState.IsValid)
+            {
+                await _editarCalificacionLN.EditarCalificacion(calificacion);
+                return RedirectToAction("Index");
+            }
+
+            return View(calificacion);
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            var calificaciones = (await _listarCalificacionesLN.ListarCalificaciones()).ToList();
+            var calificacion = calificaciones.FirstOrDefault(e => e.id_calificacion == id);
+
+            if (calificacion == null)
+                return HttpNotFound();
+
+            return View(calificacion);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            await _eliminarCalificacionLN.EliminarCalificacion(id);
+            return RedirectToAction("Index");
+        }
+
+
+       
+
+        public async Task<ActionResult> ObtenerCalificacionPorID(int id)
+        {
+            var calificaciones = await _listarCalificacionesLN.ListarCalificaciones();
+            var calificacion = calificaciones.FirstOrDefault(e => e.id_calificacion == id);
+
+            if (calificacion == null)
+                return HttpNotFound();
+
+            return View(calificacion);
+        }
+
+        [Authorize(Roles = "Estudiantes")]
+        public async Task<ActionResult> MisCalificaciones()
+        {
+            var idEstudiante = User.Identity.Name;
+            var lista = await _listarCalificacionesLN.ListarCalificacionesPorEstudianteAsync(idEstudiante);
+            return View(lista);
+        }
+    }
+}
