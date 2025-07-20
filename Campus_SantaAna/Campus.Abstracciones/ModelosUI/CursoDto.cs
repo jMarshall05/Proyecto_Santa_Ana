7b8e83572819251cc1dfd1306533bfc655b41c66
jMarshall05 <jmarshall40938@ufide.ac.cr>
2025-07-20 17:52:54 -0600

    Añadir modelo CursoDto y actualizar vista de cursos
    Se ha creado el archivo `CursoDto.cs` con propiedades para representar un curso.
    Se ha actualizado `Campus.Abstracciones.csproj` para incluir el nuevo modelo.
    Además, se han añadido dos tarjetas en `Index.cshtml` para mostrar cursos de "Inglés" y "Español".

---
+++ Campus_SantaAna/Campus.Abstracciones/ModelosUI/CursoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Campus.Abstracciones.ModelosUI
+{
+    public class CursoDto
+    {
+        public Guid IdCurso { get; set; }
+        public int MateriaId { get; set; }
+        public int GrupoId { get; set; }
+        public Guid IdProfesorId { get; set; }
+    }
+}
