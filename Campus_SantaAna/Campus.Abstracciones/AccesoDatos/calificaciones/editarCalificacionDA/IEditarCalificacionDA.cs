06a235827091b1d6f588eb87e79b63776fbba4e2
KarenVCS07 <kcamacho20881@ufide.ac.cr>
2025-08-05 18:02:31 -0600

    crud de calificaciones

--- Campus_SantaAna/Campus.Abstracciones/AccesoDatos/calificaciones/editarCalificacionDA/IEditarCalificacionDA.cs
+++ Campus_SantaAna/Campus.Abstracciones/AccesoDatos/calificaciones/editarCalificacionDA/IEditarCalificacionDA.cs
@@ -1,10 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Campus.Abstracciones.ModelosUI;
 
 namespace Campus.Abstracciones.AccesoDatos.calificaciones.editarCalificacionAD
 {
     public interface IEditarCalificacion
     {
-        Task<int> EditarCalificacion(CalificacionesDto calificacion);
+        Task<int> EditarCalificacion(int id,CalificacionesDto calificacion);
     }
 }
